f7c3f62f3a6b3cdba70772c11dce2fe8d087714e
chetan pareek <pareekchetan7891@gmail.com>
2021-05-29 06:59:55 +0530

    changed registration of hotel and user

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const user = require("./register/register_user");
 const book = require("./routes/room_booking");
 
 mongoose.connect(
-  process.env.mongoURILocal,
+  process.env.mongoURIAtlas,
   {
     useNewUrlParser: true,
     useFindAndModify: false,
